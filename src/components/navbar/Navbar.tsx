e075e8b891edf6b45e567242be7ff63baf723db5
TanyaZhecheva <jechevatanq@gmail.com>
2024-09-25 13:56:10 +0300

    add logout logic

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,28 +1,53 @@
 import { NavLink } from 'react-router-dom';
+import { useContext } from 'react';
+import { UserContext } from '../../context/UserContext';
+import { SERVER_URL } from '../../constants/ServerURL';
+import axios from 'axios';
 import './navbar.scss';
 
 function Navbar() {
+  const userContext = useContext(UserContext);
+
+  const logout = async () => {
+    const response = await axios.get(SERVER_URL('logout'), {
+      withCredentials: true,
+    });
+    if (response.status === 200) {
+      userContext?.setUser?.(null);
+      userContext?.setSession(false);
+    }
+  };
+
   return (
     <div className='navbar'>
       <div className='links'>
-        <NavLink
-          className='nav__link'
-          to='/register'
-        >
-          Register
-        </NavLink>
-        <NavLink
-          className='nav__link '
-          to='/'
-        >
-          Home
-        </NavLink>
-        <NavLink
-          className='nav__link'
-          to='/login'
-        >
-          Login
-        </NavLink>
+        {userContext?.user ? (
+          <>
+            <NavLink
+              className='nav__link '
+              to='/'
+            >
+              Home
+            </NavLink>
+            <button onClick={logout}>Logout</button>
+          </>
+        ) : (
+          <>
+            <NavLink
+              className='nav__link'
+              to='/register'
+            >
+              Register
+            </NavLink>
+
+            <NavLink
+              className='nav__link'
+              to='/login'
+            >
+              Login
+            </NavLink>
+          </>
+        )}
       </div>
     </div>
   );
