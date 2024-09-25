d773f7395f0ab45a759c2c8bdb3332b58a4b95b7
TanyaZhecheva <jechevatanq@gmail.com>
2024-09-25 13:51:51 +0300

    create two use effects  - fetching user/setting user in local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,61 @@
 import { Routes, Route } from 'react-router-dom';
-import Register from './components/register/Register';
+import { User, UserContext } from './context/UserContext.ts';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.scss';
+import Register from './components/register/Register';
 import Home from './views/home/Home';
 import Navbar from './components/navbar/Navbar';
 import Login from './components/login/Login';
 
 function App() {
+  const [user, setUser] = useState<User | null>(null);
+  const [session, setSession] = useState(false);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await axios.get('http://localhost:5000/user', {
+          withCredentials: true,
+        });
+        setUser(response.data.user);
+      } catch (error) {
+        console.error('Not authenticated');
+      }
+    };
+
+    fetchUser();
+  }, [session]);
+
+  useEffect(() => {
+    console.log(user);
+
+    if (user) {
+      localStorage.setItem('user', JSON.stringify(user));
+    } else {
+      localStorage.removeItem('user');
+    }
+  }, [user, session]);
+
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route
-          path='/register'
-          element={<Register />}
-        />
-        <Route
-          path='/'
-          element={<Home />}
-        />
-        <Route
-          path='/login'
-          element={<Login />}
-        />
-      </Routes>
+      <UserContext.Provider value={{ user, setUser, setSession }}>
+        <Navbar />
+        <Routes>
+          <Route
+            path='/register'
+            element={<Register />}
+          />
+          <Route
+            path='/'
+            element={<Home />}
+          />
+          <Route
+            path='/login'
+            element={<Login />}
+          />
+        </Routes>
+      </UserContext.Provider>
     </>
   );
 }
