567009d4c2fef4691ced082e6180dfaf5736698d
TanyaZhecheva <jechevatanq@gmail.com>
2024-09-28 11:33:21 +0300

    add components with future links(phone models) that would be implemented

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -1,15 +1,51 @@
 import { useContext } from 'react';
-import './home.scss';
 import { UserContext } from '../../context/UserContext';
+import iphoneImage from '../../assets/iphone-image.jpg';
+import samsungImage from '../../assets/samsung-image.jpg';
+import huaweiImage from '../../assets/huawei-image.jpeg';
+import TrendingPlans from '../../components/home/TrendingPlans';
+import './home.scss';
 
 function Home() {
   const userContext = useContext(UserContext);
-  console.log(userContext);
+  const images: string[] = [iphoneImage, huaweiImage, samsungImage];
+  const headings: string[] = [
+    'Iphone 15',
+    'Huawei Pura 70 ultra',
+    'Samsung s24 Ultra',
+  ];
 
   return (
     <div className='body'>
-      <h1>Hello {userContext?.user?.username}</h1>
-      <div className='user'></div>
+      <div className='blocks__container'>
+        {images.map((image, index) => (
+          <div
+            className='phone__block'
+            key={index}
+          >
+            <h1>{headings[index]}</h1>
+            <img
+              src={image}
+              alt={headings[index]}
+            />
+            <button className='btn'>Learn more</button>
+          </div>
+        ))}
+      </div>
+
+      <div className='block'>
+        <div className='heading'>
+          <h1>GENERATION`S BEST DECISION</h1>
+        </div>
+        <div className='info'>
+          <h2>
+            We believe we provide the best services. <br /> We are people`s
+            choice for better and smarter life by providing the best
+            price-performance ratio
+          </h2>
+        </div>
+      </div>
+      <TrendingPlans />
     </div>
   );
 }
