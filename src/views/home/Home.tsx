0ae9ece96963141419e3b0e7f02a14126c020d92
TanyaZhecheva <jechevatanq@gmail.com>
2024-09-29 21:56:47 +0300

    rename variables and classes for better readability

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -5,11 +5,12 @@ import samsungImage from '../../assets/samsung-image.jpg';
 import huaweiImage from '../../assets/huawei-image.jpeg';
 import TrendingPlans from '../../components/home/TrendingPlans';
 import './home.scss';
+import { Button } from 'antd';
 
 function Home() {
   const userContext = useContext(UserContext);
-  const images: string[] = [iphoneImage, huaweiImage, samsungImage];
-  const headings: string[] = [
+  const phoneImages: string[] = [iphoneImage, huaweiImage, samsungImage];
+  const phoneModels: string[] = [
     'Iphone 15',
     'Huawei Pura 70 ultra',
     'Samsung s24 Ultra',
@@ -17,26 +18,29 @@ function Home() {
 
   return (
     <div className='body'>
-      <div className='blocks__container'>
-        {images.map((image, index) => (
+      <div className='phone-models'>
+        {phoneImages.map((image, index) => (
           <div
             className='phone__block'
             key={index}
           >
-            <h1>{headings[index]}</h1>
+            <h1>{phoneModels[index]}</h1>
             <img
               src={image}
-              alt={headings[index]}
+              alt={phoneModels[index]}
             />
-            <button className='btn'>Learn more</button>
+            <Button
+              className='btn'
+              ghost
+            >
+              Learn more
+            </Button>
           </div>
         ))}
       </div>
 
-      <div className='block'>
-        <div className='heading'>
-          <h1>GENERATION`S BEST DECISION</h1>
-        </div>
+      <div className='info-block'>
+        <h1>GENERATION`S BEST DECISION</h1>
         <div className='info'>
           <h2>
             We believe we provide the best services. <br /> We are people`s
