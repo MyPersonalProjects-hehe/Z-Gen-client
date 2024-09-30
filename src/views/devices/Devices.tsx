1bb67ea2163d43f1540ccaf0be318b9709434461
TanyaZhecheva <jechevatanq@gmail.com>
2024-09-30 19:17:18 +0300

    add an image just for now implement later

---
+++ src/views/devices/Devices.tsx
@@ -0,0 +1,18 @@
+import './devices.scss';
+
+function Devices() {
+  return (
+    <div>
+      <div className='image-poster'>
+        <div className='inner__heading'>
+          <h2>You matter</h2>
+        </div>
+        <div className='outer__heading'>
+          <h2>We make life better</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Devices;
