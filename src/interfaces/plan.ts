7bab480a3433516769ae14c202dfc167e6f6b6f2
TanyaZhecheva <jechevatanq@gmail.com>
2024-09-30 19:08:55 +0300

    create an interface

---
+++ src/interfaces/plan.ts
@@ -0,0 +1,18 @@
+import { Key } from 'react';
+
+export interface Plan {
+  _id: Key | null | undefined;
+  nameOfPlan: string;
+  typeOfClient: string;
+  typeOfPlan: string;
+  minutesInBG: any;
+  minutesInEU: number;
+  discountForDevice: number;
+  MB: any;
+  MBps: number;
+  price: string;
+}
+
+export interface AllPlans {
+  plans: Plan[];
+}
