c80abfb16e8f189e1a564a3e721f45abd3c382e0
TanyaZhecheva <jechevatanq@gmail.com>
2024-10-02 22:23:59 +0300

    create in a seperate file terms

---
+++ src/components/plans/terms.ts
@@ -0,0 +1,39 @@
+export const LIST_OF_TERMS = [
+  {
+    term: 'Contract ending',
+    id: 'a',
+  },
+  {
+    term: 'Special offers',
+    id: 'b',
+  },
+  {
+    term: 'Corporate customers',
+    id: 'c',
+  },
+];
+
+export const TERMS_EXPLANATION = [
+  {
+    explanation: `Each contract has a duration of 24 months, during which customers benefit from preferential pricing. 
+ Upon the expiration of the contract term, the price will increase by 4 euros.If a customer wishes to terminate the contract prior to its expiration, a fee equivalent to three non-preferential monthly payments will be applied. 
+ * For example, if your current plan costs 20.99 euros, an early termination will result in a fee of three payments at 24.99 euros each. 
+ However, if the contract holder qualifies under our 'Impasse Policy,' the contract may be terminated at any time. The Impasse Policy covers circumstances such as the contract holder's death, a change in family status (e.g., divorce), or financial hardship, defined as a 30% reduction below the minimum wage, with official documentation required as proof.`,
+    id: 'a',
+  },
+  {
+    explanation: `Regular customers are eligible for device discounts with each signed plan. 
+These discounts can be applied to any device, and customers have the option to purchase a device through a 24-month installment plan, 
+where a fixed monthly amount will be added to the plan's tax.`,
+    id: 'b',
+  },
+  {
+    explanation: `Corporate customers are eligible for device discounts based on the number of SIM cards held. 
+ For example, a holder with three SIM cards is entitled to a total discount of 300 euros, while a holder with five SIM cards is eligible for a discount of 750 euros. Details of the discounts are provided on the first page of the contract. \n
+ Contract Termination:
+ The contract termination policy for corporate customers mirrors that of regular customers. 
+ If a corporate customer wishes to transfer a SIM card to a regular contract, this can only be done upon the expiration of the current corporate contract (after 24 months). 
+ * Early termination will incur a fee equivalent to three non-preferential monthly payments.`,
+    id: 'c',
+  },
+];
