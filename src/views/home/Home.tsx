2e2f758c3fc694b73563b9d7088aff238d7a150d
TanyaZhecheva <jechevatanq@gmail.com>
2024-10-08 21:13:06 +0300

    add config provider

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import iphoneImage from '../../assets/iphone-image.jpg';
 import samsungImage from '../../assets/samsung-image.jpg';
 import huaweiImage from '../../assets/huawei-image.jpeg';
-import { Button } from 'antd';
+import { Button, ConfigProvider } from 'antd';
 import PlanCard from '../../components/home/PlanCard';
 import { Plan } from '../../interfaces/plan';
 import { SERVER_URL } from '../../constants/ServerURL';
@@ -34,49 +34,58 @@ function Home() {
   }, []);
 
   return (
-    <div className='body'>
-      <div className='phone-models'>
-        {phoneImages.map((image, index) => (
-          <div
-            className='phone__block'
-            key={index}
-          >
-            <h1>{phoneModels[index]}</h1>
-            <img
-              src={image}
-              alt={phoneModels[index]}
-            />
-            <Button
-              className='btn'
-              ghost
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: 'white',
+        },
+      }}
+    >
+      <div className='body'>
+        <div className='phone-models'>
+          {phoneImages.map((image, index) => (
+            <div
+              className='phone__block'
+              key={index}
             >
-              Learn more
-            </Button>
-          </div>
-        ))}
-      </div>
+              <h1>{phoneModels[index]}</h1>
+              <img
+                src={image}
+                alt={phoneModels[index]}
+              />
 
-      <div className='info-block'>
-        <h1>GENERATION`S BEST DECISION</h1>
-        <div className='info'>
-          <h2>
-            We believe we provide the best services. <br /> We are people`s
-            choice for better and smarter life by providing the best
-            price-performance ratio
-          </h2>
+              <Button
+                className='btn'
+                ghost
+              >
+                Learn more
+              </Button>
+            </div>
+          ))}
+        </div>
+
+        <div className='info-block'>
+          <h1>GENERATION`S BEST DECISION</h1>
+          <div className='info'>
+            <h2>
+              We believe we provide the best services. <br /> We are people`s
+              choice for better and smarter life by providing the best
+              price-performance ratio
+            </h2>
+          </div>
+        </div>
+        <div className='plan__cards'>
+          {plans?.map((plan: Plan) => (
+            <div key={plan._id}>
+              <PlanCard
+                plan={plan}
+                isCorporate={false}
+              ></PlanCard>
+            </div>
+          ))}
         </div>
       </div>
-      <div className='plan__cards'>
-        {plans?.map((plan: Plan) => (
-          <div key={plan._id}>
-            <PlanCard
-              plan={plan}
-              isCorporate={false}
-            ></PlanCard>
-          </div>
-        ))}
-      </div>
-    </div>
+    </ConfigProvider>
   );
 }
 
