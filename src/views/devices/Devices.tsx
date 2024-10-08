b8cfbfefbc646a8d98ecfa73fe805786b71c45ae
TanyaZhecheva <jechevatanq@gmail.com>
2024-10-08 22:24:10 +0300

    change styling for devices; add a second image poster

--- src/views/devices/Devices.tsx
+++ src/views/devices/Devices.tsx
@@ -3,8 +3,10 @@ import axios from 'axios';
 import { useContext, useEffect, useState } from 'react';
 import { SERVER_URL } from '../../constants/ServerURL';
 import { Device } from '../../interfaces/device';
-import { Button } from 'antd';
+import { Button, ConfigProvider } from 'antd';
 import { DeviceContext } from '../../context/PickedDeviceContext';
+import yellowImage from '../../assets/devices-page-yellow-image.png';
+import purpleImage from '../../assets/devices-page-image.png';
 
 function Devices() {
   const [allDevices, setAllDevices] = useState([]);
@@ -30,28 +32,52 @@ function Devices() {
   };
 
   return (
-    <div>
-      <div className='image-poster'></div>
-      <div className='devices'>
-        {allDevices?.map((device: Device) => (
-          <div
-            className='device'
-            key={device._id}
-          >
-            <img
-              src={device.image}
-              alt=''
-            />
-            <h1>{device.model}</h1>
-            <h1>{device.RAM}</h1>
-            <h1>{device.price}</h1>
-            <Button onClick={() => storeChosenDevice(device)}>
-              Pick device
-            </Button>
-          </div>
-        ))}
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: '#230565',
+        },
+      }}
+    >
+      <div>
+        <div className='images-poster'>
+          <img
+            src={yellowImage}
+            alt='image'
+          />
+          <img
+            src={purpleImage}
+            alt='image'
+          />
+        </div>
+
+        <div className='devices'>
+          {allDevices?.map((device: Device) => (
+            <div
+              className='device'
+              key={device._id}
+            >
+              <h2>{device.model}</h2>
+              <img
+                src={device.image}
+                alt='device-image'
+              />
+              <h2>{device.RAM}</h2>
+              <h2>{device.price}</h2>
+              <div className='buttons'>
+                <Button
+                  className='btn'
+                  onClick={() => storeChosenDevice(device)}
+                >
+                  Pick device
+                </Button>
+                <Button className='btn'>Characteristics</Button>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </ConfigProvider>
   );
 }
 
