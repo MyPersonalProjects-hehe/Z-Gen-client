f52885ed8bfaae9003956a257ce9c43cc00ac811
TanyaZhecheva <jechevatanq@gmail.com>
2024-10-08 15:48:08 +0300

    move the config provider here

--- src/components/home/PlanCard.tsx
+++ src/components/home/PlanCard.tsx
@@ -1,5 +1,5 @@
 import './plan-card.scss';
-import { Badge, Button } from 'antd';
+import { Badge, Button, ConfigProvider } from 'antd';
 import { Plan } from '../../interfaces/plan';
 import { EuroCircleOutlined } from '@ant-design/icons';
 import { useNavigate } from 'react-router-dom';
@@ -23,51 +23,62 @@ function PlanCard({ plan, isCorporate }: PlanCardProps) {
   };
 
   return (
-    <div
-      className={
-        isCorporate
-          ? `plan__card__corporate style-wrapper`
-          : `plan__card style-wrapper`
-      }
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: 'white',
+        },
+      }}
     >
       <div
-        className='glass glass-effect-wrapper'
-        key={plan?._id}
+        className={
+          isCorporate
+            ? `plan__card__corporate style-wrapper`
+            : `plan__card style-wrapper`
+        }
       >
-        <h2 className='name-of-plan'>{plan?.nameOfPlan}</h2>
-        <h3>{plan?.cards}</h3>
-        <div className='content'>
-          <h3>
-            {isNaN(plan?.minutesInBG)
-              ? `Unlimited calls`
-              : `Minutes: ${plan?.minutesInBG}`}
-          </h3>{' '}
-          <h3>{isNaN(plan?.MB) ? `Unlimited MB` : `MB: ${plan?.MB}`}</h3>
-          <h3> MBps: {plan?.MBps}</h3>
-          <h3> {plan?.minutesInEU || ''}</h3>
-          <div className='ribbon'>
-            <Badge.Ribbon
-              text='Special'
-              color='#7462ff'
-            ></Badge.Ribbon>
+        <div
+          className='glass glass-effect-wrapper'
+          key={plan?._id}
+        >
+          <h2 className='transparent-text'>{plan?.nameOfPlan}</h2>
+          <h3>{plan?.cards}</h3>
+          <div className='content'>
+            <h3>
+              {isNaN(plan?.minutesInBG)
+                ? `Unlimited calls`
+                : `Minutes: ${plan?.minutesInBG}`}
+            </h3>{' '}
+            <h3>{isNaN(plan?.MB) ? `Unlimited MB` : `MB: ${plan?.MB}`}</h3>
+            <h3> MBps: {plan?.MBps}</h3>
+            <h3> {plan?.minutesInEU || ''}</h3>
+            <div className='ribbon'>
+              <Badge.Ribbon
+                text='Special'
+                color='#7462ff'
+              ></Badge.Ribbon>
+            </div>
+            <h3>
+              Discount for device: {plan?.discountForDevice}
+              <EuroCircleOutlined className='euro-icon' />
+            </h3>
           </div>
           <h3>
-            Discount for device: {plan?.discountForDevice}
+            Price: {plan?.price}
+            <EuroCircleOutlined className='euro-icon' /> -{' '}
+            {(Number(plan?.price) - 4).toFixed(2)}
             <EuroCircleOutlined className='euro-icon' />
           </h3>
+          <Button
+            onClick={() => navigateToSignContract(plan?._id)}
+            ghost
+            className='btn__learn__more'
+          >
+            Choose Plan
+          </Button>
         </div>
-        <h3>
-          Price: {plan?.price} <EuroCircleOutlined className='euro-icon' />
-        </h3>
-        <Button
-          onClick={() => navigateToSignContract(plan?._id)}
-          ghost
-          className='btn__learn__more'
-        >
-          Choose Plan
-        </Button>
       </div>
-    </div>
+    </ConfigProvider>
   );
 }
 
