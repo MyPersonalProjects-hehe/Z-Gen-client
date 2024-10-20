27306cf28d08073444676b1f31650db01c1af10a
TanyaZhecheva <jechevatanq@gmail.com>
2024-10-20 23:10:12 +0300

    export ant design`s modal functions into separate file

---
+++ src/helpers/modal-functions/openModal.ts
@@ -0,0 +1,50 @@
+import axios from 'axios';
+import { ContactInfo } from '../../interfaces/contactInfo';
+import { SERVER_URL } from '../../constants/ServerURL';
+
+interface ModalProps {
+  setModalText: (value: string) => void;
+  setConfirmLoading: (value: boolean) => void;
+  setOpen: (value: boolean) => void;
+  modalText: string;
+  contactInfo: ContactInfo;
+  setUploadingResult: (value: boolean) => void;
+}
+
+export const showModal = (setOpen: any) => {
+  setOpen(true);
+};
+
+export const handleOk = async ({
+  setModalText,
+  setConfirmLoading,
+  setOpen,
+  modalText,
+  contactInfo,
+  setUploadingResult,
+}: ModalProps) => {
+  try {
+    setModalText(modalText);
+    setConfirmLoading(true);
+
+    const result = await axios.post(SERVER_URL('signedContract'), contactInfo, {
+      withCredentials: true,
+    });
+
+    if (result.status === 200) {
+      setTimeout(() => {
+        setOpen(false);
+        setConfirmLoading(false);
+        setUploadingResult(true);
+      }, 3000);
+    } else {
+      setUploadingResult(false);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const handleCancel = (setOpen: any) => {
+  setOpen(false);
+};
