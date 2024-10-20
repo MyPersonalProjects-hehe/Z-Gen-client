000dd6ea5c7c81533f5cd151915e3aa60063ede6
TanyaZhecheva <jechevatanq@gmail.com>
2024-10-20 23:09:10 +0300

    add modal for async operation; add checkbox, file download button; result component

--- src/views/sing-contract/SignContractPage.tsx
+++ src/views/sing-contract/SignContractPage.tsx
@@ -6,7 +6,7 @@ import PlanCard from '../../components/home/PlanCard';
 import { SERVER_URL } from '../../constants/ServerURL';
 import { Plan } from '../../interfaces/plan';
 import ChosenDevice from '../../components/sing-contract/ChosenDevice';
-import { ConfigProvider, Steps } from 'antd';
+import { Button, Checkbox, ConfigProvider, Modal, Result, Steps } from 'antd';
 import {
   CheckCircleOutlined,
   ExclamationCircleOutlined,
@@ -14,12 +14,33 @@ import {
   SmileOutlined,
 } from '@ant-design/icons';
 import ContactDetails from '../../components/sing-contract/form/ContactDetails';
+import contract from '../../assets/ZGen Telecom Providers.pdf';
+import {
+  handleCancel,
+  handleOk,
+  showModal,
+} from '../../helpers/modal-functions/openModal';
 
 function SingContractPage() {
   const { contractId } = useParams();
   const [planCard, setPlanCard] = useState<Plan | null>(null);
   const deviceToken = localStorage.getItem('device');
   const device = deviceToken ? JSON.parse(deviceToken) : null;
+  const [toggleCheckBox, setToggleCheckBox] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [confirmLoading, setConfirmLoading] = useState(false);
+  const [modalText, setModalText] = useState(
+    'Are you sure you would like to continue?'
+  );
+  const [uploadingResult, setUploadingResult] = useState(false);
+  const [form, setForm] = useState({
+    fullName: '',
+    address: '',
+    typeOfPayment: '',
+    paperContract: '',
+    delivery: '',
+    nameOfDeliveryFirm: '',
+  });
 
   useEffect(() => {
     try {
@@ -41,80 +62,145 @@ function SingContractPage() {
     <ConfigProvider
       theme={{
         token: {
-          colorPrimary: '#5c4b5b',
+          colorPrimary: '#0e2447',
+          fontSize: 19,
         },
       }}
     >
-      <div className='sign-contract-body'>
-        <div className='progress'>
-          <Steps
-            current={1}
-            items={[
-              {
-                title: 'Finished',
-                description: 'Pick a plan',
-                icon: <CheckCircleOutlined />,
-              },
-              {
-                title: device ? 'Finished' : 'In Progress',
-                description: 'Pick a device',
-                icon: device ? (
-                  <CheckCircleOutlined />
-                ) : (
-                  <ExclamationCircleOutlined />
-                ),
-              },
-              {
-                title: 'Waiting',
-                description: 'e',
-                icon: <ExclamationCircleOutlined />,
-              },
+      {uploadingResult ? (
+        <div>
+          <Result
+            style={{ marginTop: '10rem' }}
+            status='success'
+            title='Successfully signed contract! You can view your contract update in profile menu.'
+            subTitle='Order number: 2017182818828182881 .'
+            extra={[
+              <Button
+                type='primary'
+                key='console'
+                className='btn'
+              >
+                Go Console
+              </Button>,
+              <Button
+                key='buy'
+                className='btn'
+              >
+                Buy Again
+              </Button>,
             ]}
           />
         </div>
-        <div className='picked__items'>
-          <div className='chosen-plan'>
-            <h2>Chosen plan</h2>
-            <PlanCard
-              plan={planCard}
-              isCorporate={planCard?.typeOfPlan === 'corporate'}
-            />
+      ) : (
+        <div>
+          <div className='sign-contract-body'>
+            <div className='progress'>
+              <Steps
+                current={1}
+                items={[
+                  {
+                    title: 'Finished',
+                    description: 'Pick a plan',
+                    icon: <CheckCircleOutlined />,
+                  },
+                  {
+                    title: device ? 'Finished' : 'In Progress',
+                    description: 'Pick a device',
+                    icon: device ? (
+                      <CheckCircleOutlined />
+                    ) : (
+                      <ExclamationCircleOutlined />
+                    ),
+                  },
+                  {
+                    title: 'Waiting',
+                    description: 'e',
+                    icon: <ExclamationCircleOutlined />,
+                  },
+                ]}
+              />
+            </div>
+            <div className='picked__items'>
+              <div className='chosen-plan'>
+                <h2>Chosen plan</h2>
+                <PlanCard
+                  plan={planCard}
+                  isCorporate={planCard?.typeOfPlan === 'corporate'}
+                />
+              </div>
+              <PlusCircleOutlined className='plus-icon' />
+              <div className='chosen-device'>
+                <ChosenDevice planCard={planCard} />
+              </div>
+            </div>
+
+            <div className='user__contacts'>
+              <ContactDetails
+                setForm={setForm}
+                form={form}
+              />
+            </div>
+            <div className='text-block'>
+              <h2>You are almost done!</h2>
+              <SmileOutlined className='smile-icon' />
+              <h2 className='heading'>
+                For best experience, all contracts have trial period of 14 days!
+              </h2>
+              <h2>
+                If you are not delighted with our contract you can visit the
+                nearest shop and declare a contract cancelation. The cancelation
+                is considered for processing of how we can improve our services.
+              </h2>
+              <a
+                href={contract}
+                download='Contract'
+                target='_blank'
+              >
+                <Button
+                  className='btn'
+                  type='primary'
+                  onClick={() => setToggleCheckBox(true)}
+                >
+                  Download Contract
+                </Button>
+              </a>
+            </div>
+            {toggleCheckBox && (
+              <>
+                <Checkbox>I have read all the terms and conditions.</Checkbox>
+                <Checkbox>
+                  I agree my personal information to be visible.
+                </Checkbox>
+                <Button
+                  className='btn'
+                  type='primary'
+                  onClick={() => showModal(setOpen)}
+                >
+                  Sign contract
+                </Button>
+              </>
+            )}
+
+            <Modal
+              open={open}
+              onOk={() =>
+                handleOk({
+                  setModalText: setModalText,
+                  setConfirmLoading: setConfirmLoading,
+                  setOpen: setOpen,
+                  modalText: 'Signing contract',
+                  contactInfo: form,
+                  setUploadingResult: setUploadingResult,
+                })
+              }
+              confirmLoading={confirmLoading}
+              onCancel={() => handleCancel(setOpen)}
+            >
+              <p>{modalText}</p>
+            </Modal>
           </div>
-          <PlusCircleOutlined className='plus-icon' />
-          <div className='chosen-device'>
-            <ChosenDevice planCard={planCard} />
-          </div>
         </div>
-        <div className='text-block'>
-          <SmileOutlined className='smile-icon' />
-          <h2 className='heading'>
-            For best experience, all contracts have trial period of 14 days!
-          </h2>
-          <h2>
-            If you are not delighted with our contract you can visit the nearest
-            shop and declare a contract cancelation. The cancelation is
-            considered for processing of how we can improve our services.
-          </h2>
-        </div>
-        <div className='user__contacts'>
-          <ContactDetails></ContactDetails>
-        </div>
-        <div
-          className='test
-        '
-        >
-          <h1>ssd</h1>
-          <h1>ssd</h1>
-          <h1>ssd</h1>
-          <h1>ssd</h1>
-
-          <h1>ssd</h1>
-
-          <h1>ssd</h1>
-
-          <h1>ssd</h1>
-        </div>
-      </div>
+      )}
     </ConfigProvider>
   );
 }
