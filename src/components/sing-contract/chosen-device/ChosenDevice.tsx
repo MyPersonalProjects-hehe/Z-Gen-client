b15d8d2073eef5007539abdd94ecfcc2450e2f31
TanyaZhecheva <jechevatanq@gmail.com>
2024-10-31 17:11:50 +0200

    move component into a folder

--- src/components/sing-contract/chosen-device/ChosenDevice.tsx
+++ src/components/sing-contract/chosen-device/ChosenDevice.tsx
@@ -1,10 +1,10 @@
 import './chosen-device.scss';
-import { Device } from '../../interfaces/device';
-import { Plan } from '../../interfaces/plan';
+import { Device } from '../../../interfaces/device';
+import { Plan } from '../../../interfaces/plan';
 import { EuroCircleOutlined, PlusCircleOutlined } from '@ant-design/icons';
 import { useNavigate } from 'react-router-dom';
 import { useContext, useEffect, useState } from 'react';
-import { DeviceContext } from '../../context/PickedDeviceContext';
+import { DeviceContext } from '../../../context/PickedDeviceContext';
 
 interface DeviceProps {
   planCard: Plan | null;
