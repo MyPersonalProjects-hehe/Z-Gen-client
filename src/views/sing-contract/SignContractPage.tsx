9f908c8c8b2b3ee1c0b8b180b1458e894e9edd7d
TanyaZhecheva <jechevatanq@gmail.com>
2024-10-31 17:56:25 +0200

    remove functionality for displaying picked device into parent component due to duplicating logic

--- src/views/sing-contract/SignContractPage.tsx
+++ src/views/sing-contract/SignContractPage.tsx
@@ -1,7 +1,7 @@
 import './sign-contract-page.scss';
 import axios from 'axios';
 import { useNavigate, useParams } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { SERVER_URL } from '../../constants/ServerURL';
 import { Plan } from '../../interfaces/plan';
 import ChosenDevice from '../../components/sing-contract/chosen-device/ChosenDevice';
@@ -20,21 +20,26 @@ import {
   showModal,
 } from '../../helpers/modal-functions/openModal';
 import PlanCard from '../../components/plans/plan-card/PlanCard';
+import { Device } from '../../interfaces/device';
+import { DeviceContext } from '../../context/PickedDeviceContext';
 
 function SingContractPage() {
-  const { contractId } = useParams();
   const navigate = useNavigate();
-  const deviceToken = localStorage.getItem('device');
-  const device = deviceToken ? JSON.parse(deviceToken) : null;
-  const [isDetailsComplete, setIsDetailsComplete] = useState(false);
+  const { contractId } = useParams();
+  const deviceContext = useContext(DeviceContext);
+  const [device, setDevice] = useState<Device | null>(null);
   const [planCard, setPlanCard] = useState<Plan | null>(null);
+  /**State for checking when form is completed */
+  const [isFormComplete, setIsFormComplete] = useState(false);
+  /**State for opening the modal */
   const [open, setOpen] = useState(false);
   const [toggleCheckBox, setToggleCheckBox] = useState(false);
   const [confirmLoading, setConfirmLoading] = useState(false);
   const [modalText, setModalText] = useState(
     'Are you sure you would like to continue?'
   );
-  const [uploadingResult, setUploadingResult] = useState(false);
+  /**State for checking when contract steps are finished */
+  const [isFinished, setIsFinished] = useState(false);
   const [form, setForm] = useState({
     fullName: '',
     address: '',
@@ -54,6 +59,10 @@ function SingContractPage() {
           `${SERVER_URL(`fetchPlan/${contractId}`)}`,
           { withCredentials: true }
         );
+        /**Get picked device every time global Device Context changes */
+        const deviceUnparsed = localStorage.getItem('device') || '';
+        const deviceItem = JSON.parse(deviceUnparsed);
+        setDevice(deviceItem);
         setPlanCard(response.data.plan);
       };
 
@@ -61,16 +70,18 @@ function SingContractPage() {
     } catch (error) {
       console.log(error);
     }
-  }, []);
+  }, [deviceContext?.isDevicePicked]);
 
   useEffect(() => {
-    if (isDetailsComplete) {
+    /**Add an useEffect to handle scrolling
+     * only when the form is complete */
+    if (isFormComplete) {
       window.scroll({
         top: document.body.scrollHeight,
         behavior: 'smooth',
       });
     }
-  }, [isDetailsComplete]);
+  }, [isFormComplete]);
 
   return (
     <ConfigProvider
@@ -81,7 +92,7 @@ function SingContractPage() {
         },
       }}
     >
-      {uploadingResult ? (
+      {isFinished ? (
         <div>
           <Result
             style={{ marginTop: '10rem' }}
@@ -140,7 +151,10 @@ function SingContractPage() {
               </div>
               <PlusCircleOutlined className='plus-icon' />
               <div className='chosen-device'>
-                <ChosenDevice planCard={planCard} />
+                <ChosenDevice
+                  planCard={planCard}
+                  device={device}
+                />
               </div>
             </div>
 
@@ -150,10 +164,10 @@ function SingContractPage() {
                 form={form}
                 device={device}
                 plan={planCard}
-                setIsDetailsComplete={setIsDetailsComplete}
+                setIsFormComplete={setIsFormComplete}
               />
             </div>
-            {isDetailsComplete && (
+            {isFormComplete && (
               <div className='text-block'>
                 <h2>You are almost done!</h2>
                 <SmileOutlined className='smile-icon' />
@@ -207,7 +221,7 @@ function SingContractPage() {
                   setOpen: setOpen,
                   modalText: 'Signing contract',
                   contactInfo: form,
-                  setUploadingResult: setUploadingResult,
+                  setIsFinished: setIsFinished,
                 })
               }
               confirmLoading={confirmLoading}
