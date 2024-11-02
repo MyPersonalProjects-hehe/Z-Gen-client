69755b405f116c7462341a4cc319952dba001928
TanyaZhecheva <jechevatanq@gmail.com>
2024-11-02 14:59:03 +0200

    add a scroll when rendering register component

--- src/pages/sign-up/SignUpPage.tsx
+++ src/pages/sign-up/SignUpPage.tsx
@@ -1,22 +1,36 @@
 import './sign-up-page.scss';
-import imagePoster from '../../assets/login/login-img.jpg';
+import imagePoster from '../../assets/login/new.png';
 import Login from '../../components/sign-up/login/Login';
 import Register from '../../components/sign-up/register/Register';
+import { useEffect, useState } from 'react';
 
 function SignUpPage() {
+  const [toggleScroll, setToggleScroll] = useState(false);
+
+  useEffect(() => {
+    if (toggleScroll) {
+      window.scroll({
+        top: document.body.scrollHeight,
+        behavior: 'smooth',
+      });
+    }
+  }, [toggleScroll]);
+
   return (
     <div className='sign-up-body'>
-      <div className='image__poster'>
-        <h1>Enjoy Generation`s best choice for telecom provider!</h1>
+      <div className='poster'>
+        <h1 className='text-style-wrapper'>
+          Enjoy Generation`s best choice for telecom provider!
+        </h1>
 
         <img
           src={imagePoster}
           alt='image-poster'
         />
       </div>
-      <div className='forms'>
-        <Login />
-        <Register />
+      <div className='login__register'>
+        <Login setToggleScroll={setToggleScroll} />
+        {toggleScroll && <Register />}
       </div>
     </div>
   );
