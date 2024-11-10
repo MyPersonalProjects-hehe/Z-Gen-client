462e5123c6a1a62cd4331634a37fdf369af328c2
TanyaZhecheva <jechevatanq@gmail.com>
2024-11-10 12:35:18 +0200

    change base name

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/MyPersonalProjects-hehe/Z-Gen-client/',
+  base: '/Z-Gen-client/',
 });
