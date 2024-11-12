f1462d9938eaba3d61fe9e5e68e38e546c7b7944
TanyaZhecheva <jechevatanq@gmail.com>
2024-11-12 13:44:31 +0200

    update url

--- src/constants/ServerURL.ts
+++ src/constants/ServerURL.ts
@@ -1,6 +1,2 @@
-import { config } from 'dotenv';
-
-config();
-
 export const SERVER_URL = (END_POINT: string) =>
-  `${process.env.REACT_APP_BACKEND_URL}/${END_POINT}`;
+  `${import.meta.env.VITE_BACKEND_URL}/${END_POINT}`;
