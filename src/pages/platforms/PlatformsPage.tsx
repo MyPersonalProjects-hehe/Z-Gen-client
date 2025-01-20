b2c2e5a615ea569efa8abf8b87eaa8d54a2d2930
TanyaZhecheva <jechevatanq@gmail.com>
2025-01-20 16:26:16 +0200

    create platforms page component

---
+++ src/pages/platforms/PlatformsPage.tsx
@@ -0,0 +1,227 @@
+import './platforms-page.scss';
+import strangerThingsImg from '../../assets/movie-posters/stranger_things.jpg';
+import squidGamesImg from '../../assets/movie-posters/squid_games.jpg';
+import wednesdayImg from '../../assets/movie-posters/wednesday.png';
+import { Button } from 'antd';
+import { EuroCircleOutlined } from '@ant-design/icons';
+import netflixLogo from '../../assets/logos/netflix.png';
+import hboLogo from '../../assets/logos/hbo.png';
+import disneyLogo from '../../assets/logos/disney.png';
+
+function PlatformsPage() {
+  return (
+    <div className='platforms-body'>
+      <h1 className='poster-heading'>
+        You are not ready for <span className='colored-year'>2025</span>{' '}
+      </h1>
+      <div className='movie-posters'>
+        <img
+          src={strangerThingsImg}
+          alt='img'
+        />
+        <img
+          src={wednesdayImg}
+          alt='img'
+        />
+        <img
+          src={squidGamesImg}
+          alt='img'
+        />
+      </div>
+
+      <div className='streaming-platforms'>
+        <h2>Streaming platforms</h2>
+        <div className='netflix__platform'>
+          <img
+            src={netflixLogo}
+            alt='netflix'
+          />
+          <span className='package'>
+            <h3>Basic:</h3>
+            <ul>
+              <li>Unlimited ad-free movies, TV shows, and mobile games</li>
+              <li>Watch on 1 supported device at a time</li>
+              <li>Watch in 720p (HD)</li>
+              <li>Download on 1 supported device at a time</li>
+            </ul>
+            <p>
+              3.99 <EuroCircleOutlined className='euro-icon' />
+            </p>
+            <Button
+              className='btn buy-btn'
+              type='primary'
+            >
+              Buy
+            </Button>
+          </span>
+          <span className='package'>
+            <h3>Standard:</h3>
+            <ul>
+              <li>Unlimited ad-free movies, TV shows, and mobile games</li>
+              <li>Watch on 2 supported device at a time</li>
+              <li>Watch in 1080p (Full HD)</li>
+              <li>Download on 2 supported device at a time</li>
+            </ul>
+            <p>
+              5.99 <EuroCircleOutlined className='euro-icon' />
+            </p>
+            <Button
+              className='btn buy-btn standard'
+              type='primary'
+            >
+              Buy
+            </Button>
+          </span>
+          <span className='package'>
+            <h3>Premium:</h3>
+            <ul>
+              <li>Unlimited ad-free movies, TV shows, and mobile games</li>
+              <li>Watch on 4 supported device at a time</li>
+              <li>Watch in 4K (Ultra HD) + HDR</li>
+              <li>Download on 6 supported device at a time</li>
+            </ul>
+            <p>
+              7.99 <EuroCircleOutlined className='euro-icon' />
+            </p>
+            <Button
+              className='btn buy-btn premium'
+              type='primary'
+            >
+              Buy
+            </Button>
+          </span>
+        </div>
+        <div className='hbo__platform'>
+          <img
+            src={hboLogo}
+            alt='hbo'
+          />
+          <span className='package'>
+            <h3>Basic</h3>
+            <ul>
+              <li>Stream on 2 devices at once</li>
+              <li>Full HD video resolution</li>
+              <li>30 downloads</li>
+              <li>Ad-free</li>
+            </ul>
+            <p>
+              5.99 <EuroCircleOutlined className='euro-icon' />
+            </p>
+            <Button
+              className='btn buy-btn'
+              type='primary'
+            >
+              Buy
+            </Button>
+          </span>
+          <span className='package'>
+            <h3>Standard</h3>
+            <ul>
+              <li>Stream on 4 devices at once</li>
+              <li>4K UHD & Dolby Atmos as available</li>
+              <li>100 downloads (limits apply)</li>
+              <li>Ad-free</li>
+            </ul>
+            <p>
+              7.99 <EuroCircleOutlined className='euro-icon' />
+            </p>
+            <Button
+              className='btn buy-btn standard'
+              type='primary'
+            >
+              Buy
+            </Button>
+          </span>
+          <span className='package'>
+            <h3>Premium</h3>
+            <ul>
+              <li>Stream on 6 devices at once</li>
+              <li>4K UHD & Dolby Atmos as available</li>
+              <li>200 downloads (limits apply)</li>
+              <li>Ad-free</li>
+            </ul>
+            <p>
+              7.99 <EuroCircleOutlined className='euro-icon' />
+            </p>
+            <Button
+              className='btn buy-btn premium'
+              type='primary'
+            >
+              Buy
+            </Button>
+          </span>
+        </div>
+        <div className='disney__platform'>
+          <img
+            src={disneyLogo}
+            alt='hbo'
+          />
+          <span className='package'>
+            <h3>Basic</h3>
+            <ul>
+              <li>Enjoy in up to 1080p Full HD video</li>
+              <li>Supports up to 5.1 audio</li>
+              <li>Stream on 2 devices at a time</li>
+              <li>Ad-free movies and series</li>
+              <li>Downloads on up to 10 devices</li>
+            </ul>
+            <p>
+              6.99 <EuroCircleOutlined className='euro-icon' />
+            </p>
+            <Button
+              className='btn buy-btn'
+              type='primary'
+            >
+              Buy
+            </Button>
+          </span>
+          <span className='package'>
+            <h3>Standard</h3>
+            <ul>
+              <li>Experience in up to 4K UHD and HDR video</li>
+              <li>Immersive sound with up to Dolby Atmos audio</li>
+              <li>Stream on 4 devices at a time</li>
+              <li>Ad-free movies and series</li>
+              <li>Downloads on up to 10 devices</li>
+            </ul>
+            <p>
+              9.99 <EuroCircleOutlined className='euro-icon' />
+            </p>
+            <Button
+              className='btn buy-btn standard'
+              type='primary'
+            >
+              Buy
+            </Button>
+          </span>
+          <span className='package'>
+            <h3>Extra member</h3>
+            <ul>
+              <li>
+                Enjoy Disney+ with a family member or friend even if they are
+                outside your household
+              </li>
+              <li>Stream on one device at a time</li>
+              <li>Create and access only one profile</li>
+              <li>
+                Access the same content and features as the account holder
+              </li>
+            </ul>
+
+            <p>
+              2.99 <EuroCircleOutlined className='euro-icon' />
+            </p>
+            <Button
+              className='btn buy-btn premium'
+              type='primary'
+            >
+              Buy
+            </Button>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default PlatformsPage;
