c3e002aa3cf397a4904f54e72d35ccf1e2011642
TanyaZhecheva <jechevatanq@gmail.com>
2025-03-02 16:43:06 +0200

    handle case if user cancels signing contracts with clearTimeout

--- src/helpers/modal-functions/modal-functions.ts
+++ src/helpers/modal-functions/modal-functions.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { SERVER_URL } from '../../constants/ServerURL';
 import { ContractInfo } from '../../interfaces/contractInfo';
 
-interface ModalPropsContract {
+interface ContractProps {
   setModalText: (value: string) => void;
   setConfirmLoading: (value: boolean) => void;
   setOpen: (value: boolean) => void;
@@ -14,7 +14,7 @@ interface ModalPropsContract {
   contractId: string | undefined;
 }
 
-interface ModalPropsPlatform {
+interface PlatformProps {
   setModalText: (value: string) => void;
   setConfirmLoading: (value: boolean) => void;
   setCloseIcon: (value: boolean) => void;
@@ -29,63 +29,59 @@ interface ModalPropsPlatform {
 }
 
 /**Function for signing contracts */
-export const uploadContract = async (props: ModalPropsContract) => {
+
+/**Creating a variable instead of passing it as a parameter
+ * to showModal func (antDesign func takes only 1 parameter)
+ */
+let timerId: any | null = null;
+
+export const uploadContract = async (props: ContractProps) => {
   try {
     props.setModalText(props.modalText);
     props.setConfirmLoading(true);
 
-    const uploadContractReq = await axios.post(
-      SERVER_URL('uploadContract'),
-      props.contractInfo,
-      {
+    timerId = setTimeout(async () => {
+      await axios.post(SERVER_URL('uploadContract'), props.contractInfo, {
         withCredentials: true,
-      }
-    );
-
-    if (uploadContractReq.status === 200) {
-      setTimeout(() => {
-        props.setOpen(false);
-        props.setConfirmLoading(false);
-        props.setDevicePicked((prev: boolean) => !prev);
-        props.setEligibility(false);
-        localStorage.removeItem('device');
-        localStorage.removeItem('plan');
-        props.navigate(
-          `/result/Successfully signed contract! You can view your contract update in your account./Contract id: ${props.contractId}`
-        );
-      }, 4000);
-    }
+      });
+      props.setOpen(false);
+      props.setConfirmLoading(false);
+      props.setDevicePicked((prev: boolean) => !prev);
+      props.setEligibility(false);
+      localStorage.removeItem('device');
+      localStorage.removeItem('plan');
+      props.navigate(
+        `/result/Successfully signed contract! You can view your contract update in your account./Contract id: ${props.contractId}`
+      );
+    }, 4000);
   } catch (error) {
     console.log(error);
   }
 };
 
 /**Function for purchasing streaming platforms */
-export const buyStreamingPlatform = async (props: ModalPropsPlatform) => {
+export const buyStreamingPlatform = async (props: PlatformProps) => {
   try {
     props.setConfirmLoading(true);
     props.setCloseIcon(false);
     props.setModalText('Loading');
     console.log(props.streamingPlatformInfo);
 
-    const buyStreamingPlatformReq = await axios.post(
-      SERVER_URL('buyStreamingPlatform'),
-      props.streamingPlatformInfo,
-      {
-        withCredentials: true,
-      }
-    );
-
-    if (buyStreamingPlatformReq.status === 200) {
-      setTimeout(() => {
-        props.setConfirmLoading(false);
-        props.setOpen(false);
-        props.platformContext.setIsPlatformPurchased(true);
-        props.navigate(
-          `/result/Successfully purchased streaming platform! You can view your contract in your profile. Enjoy watching your favorite movies for free!/Streaming platform - ${props.streamingPlatformInfo.platformName}`
-        );
-      }, 3000);
-    }
+    timerId = setTimeout(async () => {
+      await axios.post(
+        SERVER_URL('buyStreamingPlatform'),
+        props.streamingPlatformInfo,
+        {
+          withCredentials: true,
+        }
+      );
+      props.setConfirmLoading(false);
+      props.setOpen(false);
+      props.platformContext.setIsPlatformPurchased(true);
+      props.navigate(
+        `/result/Successfully purchased streaming platform! You can view your contract in your profile. Enjoy watching your favorite movies for free!/Streaming platform - ${props.streamingPlatformInfo.platformName}`
+      );
+    }, 4000);
   } catch (error) {
     console.log(error);
   }
@@ -97,5 +93,9 @@ export const showModal = (setOpen: any) => {
 
 export const closeModal = (openModal: any) => {
   console.log('Clicked cancel button');
+  if (timerId) {
+    clearTimeout(timerId);
+    timerId = null;
+  }
   openModal(false);
 };
