7332b5a48e7a23c944ddafc02db3fbff0ecc9d50
TanyaZhecheva <jechevatanq@gmail.com>
2025-03-02 16:41:36 +0200

    change concept for platform purchasing to be only one platform

--- src/pages/platforms/PlatformsPage.tsx
+++ src/pages/platforms/PlatformsPage.tsx
@@ -14,32 +14,17 @@ import { UserContext } from '../../context/UserContext';
 import { useNavigate } from 'react-router-dom';
 import { SERVER_URL } from '../../constants/ServerURL';
 import { StreamingPlatform } from '../../interfaces/streamingPlatform';
+import { PurchasedPlatformContext } from '../../context/PurchasedPlatformContext';
 
 function PlatformsPage() {
   const userContext = useContext(UserContext);
+  const platformContext = useContext(PurchasedPlatformContext);
   const [streamingPlatforms, setStreamingPlatforms] = useState<
     StreamingPlatform[] | []
   >([]);
   const navigate = useNavigate();
   /**Loading state */
   const [loading, setLoading] = useState(true);
-
-  const handleBuyBtn = (
-    platformName: string,
-    packageType: string,
-    price: string,
-    id: string
-  ) => {
-    userContext?.user
-      ? navigate(`/platformSign/${platformName}/${packageType}/${price}/${id}`)
-      : navigate('/signUp');
-  };
-
-  const toggleImg = (logo: string) => {
-    if (logo === 'netflixLogo') return netflixLogo;
-    if (logo === 'hboLogo') return hboLogo;
-    if (logo === 'disneyLogo') return disneyLogo;
-  };
 
   useEffect(() => {
     try {
@@ -58,6 +43,36 @@ function PlatformsPage() {
       console.log(error.message);
     }
   }, []);
+
+  const handleBuyBtn = (
+    platformNameParam: string,
+    packageTypeParam: string,
+    priceParam: string,
+    id: string
+  ) => {
+    const pickedPlatform = streamingPlatforms
+      .filter(
+        (platform: StreamingPlatform) =>
+          platform.streamingPlatform === platformNameParam
+      )
+      .map((packages: any) => {
+        const pickedPackage = packages.packageTypes[packageTypeParam];
+        return pickedPackage;
+      });
+
+    /**Passing on the platform preferences */
+    userContext?.user
+      ? navigate(
+          `/platformSign/${platformNameParam}/${packageTypeParam}/${priceParam}/${id}/${pickedPlatform}`
+        )
+      : navigate('/signUp');
+  };
+
+  const toggleImg = (logo: string) => {
+    if (logo === 'netflixLogo') return netflixLogo;
+    if (logo === 'hboLogo') return hboLogo;
+    if (logo === 'disneyLogo') return disneyLogo;
+  };
 
   return (
     <div className='platforms-body'>
@@ -79,6 +94,16 @@ function PlatformsPage() {
         />
       </div>
 
+      {platformContext?.streamingPlatform.id && (
+        <div className='not-eligible'>
+          <h1>
+            You are not eligible for purchasing a platform!
+            <br />
+            You currently have an active contract.
+          </h1>
+        </div>
+      )}
+
       <div className='streaming__platforms'>
         <h2>Streaming Platforms</h2>
         {loading ? (
@@ -97,12 +122,19 @@ function PlatformsPage() {
                   alt={skeletonImage}
                 />
                 <span className='package'>
+                  <div className='purchased__package'>
+                    {platformContext?.streamingPlatform.id ===
+                      `${platform.streamingPlatform}-Basic` && (
+                      <h3 className='badge'>Purchased</h3>
+                    )}
+                  </div>
                   <h3>Basic</h3>
                   <ul>
                     {platform.packageTypes.Basic.map(
                       (listOfPreferences: string, index) => (
                         <div key={index}>
                           {listOfPreferences.includes('Price') ? (
+                            /**Last index is price of package */
                             <p>
                               {
                                 platform.packageTypes.Basic[
@@ -119,35 +151,43 @@ function PlatformsPage() {
                     )}
                   </ul>
 
-                  <Button
-                    className='btn buy-btn'
-                    type='primary'
-                    // disabled={platformContext?.streamingPlatform.id === 'n-b'}
-                    onClick={() =>
-                      handleBuyBtn(
-                        platform.streamingPlatform,
-                        'Basic',
-                        platform.packageTypes.Basic[
-                          platform.packageTypes.Basic.length - 1
-                        ],
-                        `${platform.streamingPlatform}-Basic`
-                      )
-                    }
-                  >
-                    Buy
-                    {/* {platformContext?.streamingPlatform.id === 'n-b'
-                  ? 'Purchased'
-                  : 'Buy'} */}
-                  </Button>
+                  {!platformContext?.streamingPlatform.id && (
+                    /*If there isn`t an existing id the user is eligible
+                    for platform purchasing
+                   */
+                    <Button
+                      className='btn buy-btn basic'
+                      type='primary'
+                      onClick={() =>
+                        handleBuyBtn(
+                          platform.streamingPlatform,
+                          'Basic',
+                          platform.packageTypes.Basic[
+                            platform.packageTypes.Basic.length - 1
+                          ],
+                          `${platform.streamingPlatform}-Basic`
+                        )
+                      }
+                    >
+                      Buy
+                    </Button>
+                  )}
                 </span>
 
                 <span className='package'>
+                  <div className='purchased__package'>
+                    {platformContext?.streamingPlatform.id ===
+                      `${platform.streamingPlatform}-Standard` && (
+                      <h3 className='badge'>Purchased</h3>
+                    )}
+                  </div>
                   <h3>Standard</h3>
                   <ul>
                     {platform.packageTypes.Standard.map(
                       (listOfPreferences: string, index) => (
                         <div key={index}>
                           {listOfPreferences.includes('Price') ? (
+                            /**Last index is price of package */
                             <p>
                               {
                                 platform.packageTypes.Standard[
@@ -163,35 +203,41 @@ function PlatformsPage() {
                       )
                     )}
                   </ul>
-                  <Button
-                    className='btn buy-btn standard'
-                    type='primary'
-                    // disabled={platformContext?.streamingPlatform.id === 'n-b'}
-                    onClick={() =>
-                      handleBuyBtn(
-                        platform.streamingPlatform,
-                        'Standard',
-                        platform.packageTypes.Standard[
-                          platform.packageTypes.Standard.length - 1
-                        ],
-                        `${platform.streamingPlatform}-Standard`
-                      )
-                    }
-                  >
-                    Buy
-                    {/* {platformContext?.streamingPlatform.id === 'n-b'
-                  ? 'Purchased'
-                  : 'Buy'} */}
-                  </Button>
+
+                  {!platformContext?.streamingPlatform.id && (
+                    <Button
+                      className='btn buy-btn standard'
+                      type='primary'
+                      onClick={() =>
+                        handleBuyBtn(
+                          platform.streamingPlatform,
+                          'Standard',
+                          platform.packageTypes.Standard[
+                            platform.packageTypes.Standard.length - 1
+                          ],
+                          `${platform.streamingPlatform}-Standard`
+                        )
+                      }
+                    >
+                      Buy
+                    </Button>
+                  )}
                 </span>
 
                 <span className='package'>
+                  <div className='purchased__package'>
+                    {platformContext?.streamingPlatform.id ===
+                      `${platform.streamingPlatform}-Premium` && (
+                      <h3 className='badge'>Purchased</h3>
+                    )}
+                  </div>
                   <h3>Premium</h3>
                   <ul>
                     {platform.packageTypes.Premium.map(
                       (listOfPreferences: string, index) => (
                         <div key={index}>
                           {listOfPreferences.includes('Price') ? (
+                            /**Last index is price of package */
                             <p>
                               {
                                 platform.packageTypes.Premium[
@@ -207,26 +253,25 @@ function PlatformsPage() {
                       )
                     )}
                   </ul>
-                  <Button
-                    className='btn buy-btn premium'
-                    type='primary'
-                    // disabled={platformContext?.streamingPlatform.id === 'n-b'}
-                    onClick={() =>
-                      handleBuyBtn(
-                        platform.streamingPlatform,
-                        'Premium',
-                        platform.packageTypes.Premium[
-                          platform.packageTypes.Premium.length - 1
-                        ],
-                        `${platform.streamingPlatform}-Premium`
-                      )
-                    }
-                  >
-                    Buy
-                    {/* {platformContext?.streamingPlatform.id === 'n-b'
-                  ? 'Purchased'
-                  : 'Buy'} */}
-                  </Button>
+
+                  {!platformContext?.streamingPlatform.id && (
+                    <Button
+                      className='btn buy-btn premium'
+                      type='primary'
+                      onClick={() =>
+                        handleBuyBtn(
+                          platform.streamingPlatform,
+                          'Premium',
+                          platform.packageTypes.Premium[
+                            platform.packageTypes.Premium.length - 1
+                          ],
+                          `${platform.streamingPlatform}-Premium`
+                        )
+                      }
+                    >
+                      Buy
+                    </Button>
+                  )}
                 </span>
               </div>
             )
