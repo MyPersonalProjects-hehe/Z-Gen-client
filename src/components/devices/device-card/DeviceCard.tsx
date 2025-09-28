785cea73539219f4b9cb25d34e28f0653f002f03
TanyaZhecheva <jechevatanq@gmail.com>
2025-09-28 12:09:23 +0300

    change text

--- src/components/devices/device-card/DeviceCard.tsx
+++ src/components/devices/device-card/DeviceCard.tsx
@@ -61,7 +61,7 @@ function DeviceCard({ device }: DeviceProp) {
             })
           }
         >
-          Store device
+          Add to cart
         </Button>
         <Button
           className='btn'
